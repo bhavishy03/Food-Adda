33bd3e8738edf51d30bfce6b6af1c9fd0342dd9e
bhavishy03 <bhavishyrajak2036@gmail.com>
2025-07-16 15:22:04 +0530

    Update axios.js

--- food-delivery-frontend/src/utils/axios.js
+++ food-delivery-frontend/src/utils/axios.js
@@ -1,1 +1,12 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https://food-adda.onrender.com/api",
+  timeout: 5000,
+  headers: {
+    "Content-Type": "application/json"
+  }
+});
+
+export default instance;
 
