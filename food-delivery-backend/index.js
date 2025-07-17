5cb4af6d35630a80bb1ee9bc1a29a9a9c6939e61
bhavishy03 <bhavishyrajak2036@gmail.com>
2025-07-17 13:41:48 +0530

    Update index.js

--- food-delivery-backend/index.js
+++ food-delivery-backend/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
 const addressRoutes = require('./routes/addressRoutes');
+const dishRoutes = require("./routes/dishRoutes");
 
 require("dotenv").config();
 
